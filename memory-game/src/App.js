5aa7b153b1a4a7a5b002f9ec5251109dba107764
erkartali <erkartali@gmail.com>
2020-11-19 11:39:47 -0600

    created react app

---
+++ memory-game/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; 
+
+class App extends Component {
+  render () {
+    return (
+      <div >
+        <p>dicks</p>
+      </div>
+    );
+  }
+}
+
+
+export default App;
